c3d10e841e7f6ee8f5afbf6683319810d4cff2a1
ThiagoKalac <thiago.rds@outlook.com.br>
2024-05-24 16:45:48 -0300

    feat: implementado service e controler do usuario, e adicionado busca de info no servidor saib

---
+++ src/controllers/usuario/infoUsuario.Controller.ts
@@ -0,0 +1,11 @@
+import { Response, Request } from "express";
+import { infoUsuarioService } from "../../services/usuario/infoUsuario.Service";
+
+
+const infoUsuarioController = async (req:Request, res:Response) => {
+    const cpf:string = req.params.cpf;
+    const informaçõesUsuario = await infoUsuarioService(cpf);
+    return res.status(200).json({mensagem: informaçõesUsuario})
+}
+
+export {infoUsuarioController};
