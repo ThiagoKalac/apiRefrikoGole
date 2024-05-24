0bde526629d3fb9d669efaba045a5c7387117b21
ThiagoKalac <thiago.rds@outlook.com.br>
2024-05-24 08:57:46 -0300

    feat: add dependencia yup e criado arquitetura de pastar, e tratativas para erro.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import 'express-async-errors';
+import { usuarioRouter } from "./routes/usuario.route";
+import { tratarError } from "./error/tratarError";
 
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(tratarError);
 
+app.use('/usuario', usuarioRouter);
 
 
 export {app}
